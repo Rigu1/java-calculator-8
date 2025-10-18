e97cf3a94a04efa4b74b8fff0b41ba6bc9223f22
rigu1 <dlsrbtla@gmail.com>
2025-10-18 22:14:16 +0900

    feat: 합계 출력하기 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,20 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.Delimiters;
+import calculator.domain.InputSplitter;
+import calculator.domain.Operands;
+import calculator.ui.InputView;
+import calculator.ui.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputSplitter inputSplitter = InputSplitter.from(InputView.readLine());
+        Delimiters delimiters = Delimiters.from(inputSplitter.getHeader());
+        Operands operands = Operands.of(delimiters.getDelimiters(), inputSplitter.getExpression());
+
+        Calculator calculator = new Calculator(operands);
+
+        OutputView.printResult(calculator.add());
     }
 }
---
+++ src/main/java/calculator/ui/OutputView.java
@@ -0,0 +1,8 @@
+package calculator.ui;
+
+public class OutputView {
+
+    public static void printResult(long result) {
+        System.out.println("결과 : " + result);
+    }
+}
